f855ff37e17420b46d5800ad38e9b96fbd53810a
wilfred05777@gmail.com <wilfred05777@gmail..com>
2022-11-21 05:54:31 +0800

    7:35_ 5. Implementing Tab Bar View for Login and SignUp

--- lib/authentication/auth_screen.dart
+++ lib/authentication/auth_screen.dart
@@ -10,10 +10,36 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Welcome"),
-      ),
-    );
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            flexibleSpace: Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Colors.cyan, Colors.amber],
+                      begin: FractionalOffset(0.0, 0.0),
+                      end: FractionalOffset(1.0, 0.0),
+                      stops: [0.0, 1.0],
+                      tileMode: TileMode.clamp)),
+            ),
+            title: const Text(
+              'iFood',
+              style: TextStyle(
+                  fontSize: 60, color: Colors.white, fontFamily: 'Lobster'),
+            ),
+            centerTitle: true,
+            bottom: const TabBar(tabs: [
+              Tab(
+                icon: Icon(Icons.lock, color: Colors.white),
+                text: "Login",
+              ),
+              Tab(
+                icon: Icon(Icons.person, color: Colors.white),
+                text: "Register",
+              )
+            ]),
+          ),
+        ));
   }
 }
